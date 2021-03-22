06df0f3fb18e0f2e2fd0b1cdaec5cb14c079ef4b
quietcreator <49174574+quietcreator@users.noreply.github.com>
2021-03-22 20:13:43 +0300

    firstapp

---
+++ src/components/UI/Loader/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Loader.css'
+
+const Loader = () => (
+  <div className="center">
+    <div className="Loader">
+      <div /><div />
+    </div>
+  </div>
+)
+
+export default Loader
